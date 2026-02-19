766f809a58d9f88f9ae01d2b2ad87784ee067ee0
Pablo Pena <pablo_penna7@hotmail.com>
2026-02-19 07:06:09 +0100

    doc: add comment regarding syntax

--- test/code.s
+++ test/code.s
@@ -1,18 +1,19 @@
+@ https://stackoverflow.com/questions/21652884/is-the-hash-required-for-immediate-values-in-arm-assembly/51987780#51987780
+@ This next line allows some syntax capabilities like not prependin literals with #
+.syntax unified 
 .global _start
 
-
 _start:
-
 	@ write syscall
-	mov r7, #4
-	mov r0, #1
+	mov r7, 4
+	mov r0, 1
 	ldr r1, =hello
-	mov r2, #12 @ length of the string contained in the 'hello' address
+	mov r2, 12 @ length of the string contained in the 'hello' address
 	svc 0
 
 	@ exit syscall	
-	mov r7, #1
-	mov r0, #0
+	mov r7, 1
+	mov r0, 0
 	svc 0
 
 	.data
