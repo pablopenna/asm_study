2dfff72ed6c75f8f9cf3adc4b1bf2eff1af21efa
Pablo Pena <pablopenna@users.noreply.github.com>
2026-02-23 17:43:19 +0800

    Otter exercise template

--- arm/armasm.com/find_the_otter/otter.asm
+++ arm/armasm.com/find_the_otter/otter.asm
@@ -1,12 +1,23 @@
-.global _start
+@ Program 3:  Find the Otter 
+@ https://www.armasm.com/docs/registers-and-memory/find-the-otter/
+@
+@ Reads pre-loaded values in the registers and writes 
+@ OTTER to STDOUT 
 
-.text
+.global _start 
 
-  _start:
-    mov r7, #1
-    mov r0, #99
-    svc 0
+_start: 
+    @@@@@@@@@@@@@@@@ Begin Register Preload 
+    mov     r0, #'P' 
+    mov     r1, #'O'
+    mov     r2, #'I'
+    mov     r3, #'U'
+    mov     r4, #'Y'
+    mov     r5, #'T'
+    mov     r6, #'R'
+    mov     r7, #'E'
+    mov     r8, #'W'
+    mov     r9, #'Q'
+    @@@@@@@@@@@@@@@@ End Register Preload 
 
-.data
-
-  outstr: .ascii "          \n"
+    @ Write your program here!!! 
