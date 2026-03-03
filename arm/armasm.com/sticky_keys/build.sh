96c7cdfb93960f9ead60ce0259f376320a8c0f45
Pablo Pena <pablo_penna7@hotmail.com>
2026-03-03 09:25:57 +0100

    setup sticky keys exercise

---
+++ arm/armasm.com/sticky_keys/build.sh
@@ -0,0 +1,2 @@
+arm-linux-gnueabi-as -g -o main.o main.asm
+arm-linux-gnueabi-ld -o main.exe main.o
---
+++ arm/armasm.com/sticky_keys/main.asm
@@ -0,0 +1,20 @@
+@ Program 6:  Hello World Revisited
+@ https://www.armasm.com/docs/branches-and-conditionals/sticky-keyboard/
+@
+@ My keyboard sucks. Thanks Apple. The keys stick badly. (They don't really)
+@ Write a program that takes a string and removes the duplicate letters. 
+@
+@
+
+.include "write.asm" 
+.include "exit.asm" 
+
+.global _start 
+
+_start: 
+   @ Your code here 
+
+.data 
+instr:      .ascii      "I jjjjust   waaaaant thhhis stuppppid " 
+            .asciz      "tttthinggggg tooooo wwwwworrrrrrrk!!!!!!!\n"
+outstr:     .fill       128, 1, 0     @ Reserve 128 bytes
