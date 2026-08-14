af4a086f14b815f27af5a01ac7e8169d93dd68ce
Pablo Peña <pablo_penna7@hotmail.com>
2026-08-14 13:30:23 +0800

    string length macro for library

--- x86/low_level_book/basic_library/lib.s
+++ x86/low_level_book/basic_library/lib.s
@@ -1,3 +1,9 @@
+; From the book: Any function can accept an unlimited number of arguments. The 
+; first six arguments are passed in rdi, rsi, rdx, rcx, r8, and r9, respectively. 
+; The rest is passed on to the stack in reverse order.
+;
+; Callee-saved registers must be restored by the procedure being called. So, if it needs to change them, it has to change them back.
+; These registers are callee-saved: rbx, rbp, rsp, r12-r15, a total of seven registers.
 section .text
 
 ; exit code in rdi as parameter - matches the syscall so we do not touch rdi
@@ -6,3 +12,18 @@ exit:
     ; no need to care about the stack as the program is going to exit so nothing after this
     mov rax, 60
     syscall
+
+global string_length
+; in - rdi contains pointer to string
+; out - rax contains length of the string
+string_length:
+    xor rax, rax
+
+    .loop:
+    cmp byte[rdi+rax], 0
+    je .end
+    inc rax
+    jmp .loop
+
+    .end:
+    ret
--- x86/low_level_book/basic_library/main.s
+++ x86/low_level_book/basic_library/main.s
@@ -1,12 +1,15 @@
 section .text
 global _start
 extern exit
+extern string_length
 
 _start:
-  mov rdi, 69
+  mov rdi, codes
+  call string_length
+
+  mov rdi, rax
   call exit
 
 section .data
-codes:
-    db      '0123456789ABCDEF'
+codes: db '0123456789ABCDEF', 0x0
 newline: db 10
